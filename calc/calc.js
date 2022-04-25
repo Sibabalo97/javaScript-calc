1337b45acf81077338c4db9fc53eb8a5e483ed50
sibabalo97 <sibabalomaqiy@gmail.com>
2022-04-26 00:27:09 +0200

    javaScript

---
+++ calc/calc.js
@@ -0,0 +1,22 @@
+var inputLabel = document.getElementById("inputLabel");
+
+        function pushBtn(obj) {
+            var pushed = obj.innerHTML;
+
+        if(pushed == "=") {
+            // calculate
+            inputLabel.innerHTML = eval(inputLabel.innerHTML);
+    
+    
+        } else if(pushed == "AC") {
+            // all clear
+            inputLabel.innerHTML ='0';
+            
+        } else {
+            if(inputLabel.innerHTML =='0') {
+            inputLabel.innerHTML = pushed;
+        } else {
+            inputLabel.innerHTML += pushed;
+        }
+    }
+}
